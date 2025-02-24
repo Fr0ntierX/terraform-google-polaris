091eac0d16f3ca47645f7b0701c29b341c8bb28e
Karol Legut <karol.legut121@gmail.com>
2025-02-24 13:44:59 +0100

    feat: add example Terraform configuration with variables, outputs, and provider setup

---
+++ examples/main.tf
@@ -0,0 +1,22 @@
+provider "google" {
+  project = var.project_id
+  region  = var.region
+}
+
+module "polaris" {
+  source = "../"
+
+  project_id                = var.project_id
+  goog_cm_deployment_name   = var.goog_cm_deployment_name
+  region                    = var.region
+  workload_image            = var.workload_image
+  service_account           = var.service_account
+  zone                      = var.zone
+  polaris_proxy_port        = var.polaris_proxy_port
+  networks                  = var.networks
+  sub_networks              = var.sub_networks
+  external_ips              = var.external_ips
+  polaris_proxy_image       = var.polaris_proxy_image
+  workload_port             = var.workload_port
+  workload_env_vars         = var.workload_env_vars
+}
---
+++ examples/outputs.tf
@@ -0,0 +1,19 @@
+output "instance_self_link" {
+  value       = module.polaris.instance_self_link
+}
+
+output "instance_zone" {
+  value       = module.polaris.instance_zone
+}
+
+output "instance_machine_type" {
+  value       = module.polaris.instance_machine_type
+}
+
+output "instance_nat_ip" {
+  value       = module.polaris.instance_nat_ip
+}
+
+output "instance_network" {
+  value       = module.polaris.instance_network
+}
